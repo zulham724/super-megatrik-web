271a8085aac68eb87ce07e397a814c4c60377cd7
ZULHAM AZWAR ACHMAD <zulham724@students.unnes.ac.id>
2018-12-07 14:45:20 +0700

    update api driver

--- app/Models/Servicecategory.php
+++ app/Models/Servicecategory.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\CrudTrait;
 
-class Servicecategory extends Model
+class ServiceCategory extends Model
 {
     use CrudTrait;
 
